213f306afd582a88eb999c0968809776dc192b9f
eccentricality <youngmin13@gmail.com>
2021-09-09 10:21:49 -0400

    seeds update

--- db/seed.sql
+++ db/seed.sql
@@ -1,27 +1,50 @@
-USE employee_db;
+USE jediDB;
 
-INSERT INTO department (dept_name)
+INSERT INTO departments (name)
 VALUES
-    ('Sales'),
-    ('Software'),
-    ('Finance'),
-    ('Legal');
+    ('Council'),
+    ('Assault Team'),
+    ('Exploration Corps'),
+    ('Temple Guardians'),
+    ('Academy');
 
 INSERT INTO role (title, salary, dept_id)
 VALUES
-    ('Sales Manager', 150000, 1),
-    ('Sales Rep', 90000, 1),
-    ('Legal PM', 250000, 4),
-    ('Attorney', 150000, 4),
-    ('Jr Developer', 90000, 2),
-    ('Sr Developer', 130000, 2),
-    ('Acct Manager', 150000, 3),
-    ('Payroll', 100000, 3);
+    ('Grand Master', 1000000, 1),
+    ('Master of the Order', 800000, 1),
+    ('Jedi Council Member', 600000, 1),
+    ('Jedi Master', 400000, 2),
+    ('Jedi Sentinel', 350000, 2),
+    ('Jedi Guardian', 350000, 2),
+    ('Jedi Consular', 350000, 3),
+    ('Jedi Knight', 250000, 3),;
+    ('Jedi Service Corps', 125000, 4),;
+    ('Padawan', 90000, 5),;
+    ('Youngling', 50000, 5);
 
-INSERT INTO employee (first_name, last_name, mgr_id)
+INSERT INTO epmloyee (first_name, last_name, role_id, manager_id)
 VALUES
-    ('Anakin', 'Skywalker', NULL),
-    ('Ahsoka', 'Tano', NULL),
-    ('Mace', 'Windu', 3),
-    ('Master', 'Yoda', 1),
-    ('Obi-Wan', 'Kenobi', 5);
+    ('Yoda', '', 1, 5),
+    ('Mace', 'Windu', 2, 4),
+    ('Plo', 'Koon', 3, 3),
+    ('Ki-Adi', 'Mundi', 3, 2),
+    ('Saesee', 'Tiin', 3, 1),
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES
+    ('Qui-Gon', 'Jinn', 4),
+    ('Obi-Wan', 'Kenobi', 4),
+    ('Morrit', 'Ch\'gally', 5),
+    ('Bastila', 'Shan', 5),
+    ('Shaak', 'Ti', 6),
+    ('Kit', 'Fisto', 6),
+    ('Anakin', 'Skywalker', 6),
+    ('Luminara', 'Unduli', 7),
+    ('Barriss', 'Offee', 7),;
+    ('Ezra', 'Bridger', 8),;
+    ('Ven', 'Zallow', 8),;
+    ('Jocasta', 'Nu', 9),;
+    ('Ferren', 'Barr', 9),;
+    ('Ahsoka', 'Tano', 10),;
+    ('Luke', 'Skywalker', 11),;
+    ('Leia', 'Skywalker', 11);
